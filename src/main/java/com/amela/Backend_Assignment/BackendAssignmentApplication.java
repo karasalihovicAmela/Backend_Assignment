31aa3757c1cb70b0e3f36dd384a8fc9c893488e1
Amela Karasalihovic <amela.karasalihovic22@gmail.com>
2021-04-08 12:44:19 +0200

    Adding project to github

---
+++ src/main/java/com/amela/Backend_Assignment/BackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.amela.Backend_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAssignmentApplication.class, args);
+	}
+
+}
